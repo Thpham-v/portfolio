8fa40bf6f2e10241d0c0fb002b21aa1364160e37
Thibault P.V <Reus@reus186.home>
2023-05-11 16:51:56 +0200

    Updates

--- components/canvas/index.js
+++
@@ -1,6 +0,0 @@
-import Mail from "./Mail";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
-
-export { Mail, BallCanvas, ComputersCanvas, StarsCanvas };
