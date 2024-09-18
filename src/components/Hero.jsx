4776a84d881e041aafff672bf1514645734fb7e3
Thibault P.V <Reus@reus186.home>
2024-09-18 03:38:47 +0200

    fix(frontend): fix color purple

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,6 +9,11 @@ const Hero = () => {
 
       <div className={`${styles.paddingX} relative inset-0 top-[140px] max-w-7xl mx-auto flex flex-row items-start gap-5 `}>
 
+        <div className="flex flex-col justify-center items-center mt-10 max-sm:mt-[38%]">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
         <div className="relative flex flex-col max-sm:mb-[200px]">
           <div className="flex max-sm:flex-col-reverse max-sm:flex justify-center items-center w-full">
 
